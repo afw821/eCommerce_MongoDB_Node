5b93c1d3905937b324a8c6d6e96478a8c3b5d74d
Alexander Watkins <afw821@gmail.com>
2019-02-20 01:58:49 -0500

    I added a select quantity box to the fishing section

--- public/js/ajax-logic.js
+++ public/js/ajax-logic.js
@@ -93,6 +93,23 @@ $(function () {
             .then(function (res) {
                 console.log(res);
                 let closeText = '<span aria-hidden="true">&times;</span>';
+                let userQuantity = `<div class="form-row align-items-center">
+                <div class="col-auto my-1">
+                  <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
+                  <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                    <option selected>Quantity</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                  </select>
+                </div>`
                 $(res).each(function (i) {
                     console.log(res[i].productname);
                     let productName = res[i].productname;
@@ -118,11 +135,12 @@ $(function () {
                     let cardHead = $('<div>', {
                         class: 'card-header',
                         value: productName,
-                        text: productName,
+                        text: `${productName} $${price}`,
                         appendTo: cardDiv
                     });
                     let cardBody = $('<div>', {
                         class: 'card-body',
+                        
                         appendTo: cardDiv
                     });
                     let cardImage = $('<img>', {
@@ -130,6 +148,7 @@ $(function () {
                         src: imgUrl,
                         appendTo: cardBody
                     });
+                   
                     let closeButton = $('<button>', {
                         type: 'button',
                         class: 'close',
@@ -144,15 +163,22 @@ $(function () {
                         },
                         appendTo: cardBody,
                         html: closeText
-                    })
+                    });
+                  
                     let cardTitle = $('<h5>', {
                         class: 'card-title',
-                        text: departmentName,
+                        text: `Category: ${departmentName}`,
                         appendTo: cardBody
                     });
                     let cardText = $('<p>', {
                         class: "card-text",
                         text: itemDescription,
+                        
+                        appendTo: cardBody
+                    });
+                    let selectBox = $('<div>', {
+                        class: 'd-flex justify-content-center',
+                        html: userQuantity,
                         appendTo: cardBody
                     });
                     let buttonLink = $('<a>', {
@@ -164,7 +190,7 @@ $(function () {
 
                     let cardFooter = $('<div>', {
                         class: "card-footer text-muted",
-                        text: "2 Days ago",
+                        
                         appendTo: cardDiv
                     });
                     $('.card-div-fishing').append(cardDiv);
