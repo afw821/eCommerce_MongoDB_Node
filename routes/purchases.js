a5f3a457a3864d9f95234a05ae662f2fa991f671
Alexander Watkins <afw821@gmail.com>
2019-05-14 22:59:06 -0400

    I updated the purchase api routes

--- routes/purchases.js
+++ routes/purchases.js
@@ -9,6 +9,19 @@ router.get('/', async function (req, res) {
     res.send(rentals);
 });
 
+router.post('/', async function (req, res) {
+    const result = validate(req.body);
+    if (result.error) return res.status(400).send(error.details[0].message);
+
+    const buyer = await Buyer.findById(req.body.buyerId);
+    if(!buyer) return res.status(400).send('Invalid user');
+
+    const product = await Product.findById(req.body.productId);
+    if(!product) return res.status(400).send('Product wasn not located');
+
+    if(product.stockquantity === 0) return res.status(400).send('Product not in stock');
+});
+
 
 
 
