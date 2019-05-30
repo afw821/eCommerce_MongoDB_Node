6a3a321c272015fdd090bbc76f822e5da21a034d
Alexander Watkins <afw821@gmail.com>
2019-05-29 21:45:47 -0400

    I finished the auth function and exported it for use to protect routes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,23 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
+//THIS FUNCTION IS USED TO PROTECT ROUTES WITH JWT
 function auth(req, res, next) {
     //get token from request header
     const token = req.header('x-auth-token');
     //if token doesn't exist send access denied back to the client
-    if (!token) res.status(401).send('Access Denied. No Token provided');
+    if (!token) return res.status(401).send('Access Denied. No Token provided');
     try {
         //if token does exist verify valid token
         //2nd argument is Private Key for decoding the token
         const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+        //user from Buyer Model
+        req.user = decoded;
+        //pass control to next middleware function
+        next();
     } catch (error) {
         res.status(400).send('invalid token', error);
     }
 
 }
+
+module.exports = auth
