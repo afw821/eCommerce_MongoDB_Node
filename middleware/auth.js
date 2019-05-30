a20f12007f2203ddb7737301433b7619130e3ba9
Alexander Watkins <afw821@gmail.com>
2019-05-29 21:35:49 -0400

    I verifyied the jwt and implemented a try catch block

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,16 @@
-
-
+const jwt = require('jsonwebtoken');
+const config = require('config');
 function auth(req, res, next) {
+    //get token from request header
     const token = req.header('x-auth-token');
-    if(!token) res.status(401).send('Access Denied. No Token provided');
+    //if token doesn't exist send access denied back to the client
+    if (!token) res.status(401).send('Access Denied. No Token provided');
+    try {
+        //if token does exist verify valid token
+        //2nd argument is Private Key for decoding the token
+        const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+    } catch (error) {
+        res.status(400).send('invalid token', error);
+    }
+
 }
