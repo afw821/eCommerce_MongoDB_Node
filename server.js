b5180c7971da1683df2ae4ca90a702fdb6a1a5a4
Alexander Watkins <afw821@gmail.com>
2019-06-06 23:39:50 -0400

    I extracted the db logic and middleware fucntions and added them to their own separate module

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const app = express();
 const auth = require('./routes/auth');
 const PORT = process.env.PORT || 8891;
 console.log(`Environment: ${process.env.NODE_ENV}`);
-const products = require('./routes/products');
-const buyers = require('./routes/buyers');
+// const products = require('./routes/products');
+// const buyers = require('./routes/buyers');
 const htmlroutes = require('./routes/html-routes')(app);
 //Middleware functions
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/api/products', products);
-app.use('/api/buyer', buyers);
-app.use('/api/auth', auth);
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/api/products', products);
+// app.use('/api/buyer', buyers);
+// app.use('/api/auth', auth);
 console.log(`Application Name: ${config.get('name')}`);
 console.log(`Mail Server: ${config.get('mail.host')}`);
 //have to set this environment variable first
---
+++ startup/routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const products = require('./routes/products');
+const buyers = require('./routes/buyers');
+const auth = require('./routes/auth');
+const path = require('path');
+module.exports = function (app) {
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use('/api/products', products);
+    app.use('/api/buyer', buyers);
+    app.use('/api/auth', auth);
+}
